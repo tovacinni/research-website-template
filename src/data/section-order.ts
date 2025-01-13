b0dde9d86cc692d744b9e3ec9f9d70015715cc6a
Chad <sclanga315@gmail.com>
2025-01-13 01:10:28 -0500

    Publications hidden 2

--- src/data/section-order.ts
+++ src/data/section-order.ts
@@ -2,14 +2,14 @@ export enum Section {
   Education = "education",
   Experience = "experience",
   Portfolio = "portfolio",
-  //Publication = "publication",
+  Publication = "publication",
   News = "news",
 }
 
 export const sectionOrder = [
   Section.News,
   Section.Education,
-  //Section.Publication,
+  Section.Publication,
   Section.Experience,
   Section.Portfolio,
 ];
