37b85cb1f457d364996f1c14bc51865559a42e8b
kotashimomura <admin@MacBook-Pro.local>
2025-02-08 19:44:38 +0900

    fix

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -18,22 +18,22 @@ export interface AboutMe {
 }
 
 export const aboutMe: AboutMe = {
-  name: "Jane R. Smith",
-  title: "Ph.D. Candidate",
-  institution: "Stanford University",
+  name: "Kota Shimomura",
+  title: "Ph.D. Student",
+  institution: "Chubu University",
   // Note that links work in the description
   description:
-    "I'm a final-year <a href='https://www.stanford.edu'>PhD candidate</a> working at the intersection of causal inference and machine learning. My research focuses on developing robust, interpretable systems that can reason about cause and effect in complex environments.",
-  email: "______@stanford.edu",
+    "I'm a first-year <a href='https://www.chubu.ac.jp/english/'>PhD candidate</a> working at the computer vision and vision and language. My research focuses on Autonomous Driving, ADAS system.",
+  email: "shimo@mprg.cs.chubu.ac.jp",
   imageUrl:
     "https://images.unsplash.com/photo-1581481615985-ba4775734a9b?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  googleScholarUrl: "https://scholar.google.com/citations?user=bWtMl_MAAAAJ",
-  githubUsername: "janesmith",
-  linkedinUsername: "janesmith",
-  twitterUsername: "janesmith",
-  blogUrl: "https://",
-  cvUrl: "https://",
-  institutionUrl: "https://www.stanford.edu",
+  googleScholarUrl: "https://scholar.google.com/citations?user=0A_HXBwAAAAJ&hl=ja",
+  githubUsername: "kotashimomura",
+  linkedinUsername: "kotashimomura",
+  twitterUsername: "Naruch11",
+  // blogUrl: "https://",
+  // cvUrl: "https://",
+  institutionUrl: "https://www.chubu.ac.jp/english/",
   // altName: "",
   // secretDescription: "I like dogs.",
 };
